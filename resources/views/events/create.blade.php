c147cef78ecd6a6de3905f880ac42e5fa1a6c00a
enespirevadev <enespireva9@gmail.com>
2023-09-18 15:12:54 +0200

    Implements Events CRUD // Adds TIKETANI Logo

---
+++ resources/views/events/create.blade.php
@@ -0,0 +1,24 @@
+<x-app-layout>
+    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+        <form method="POST" action="{{ route('events.store') }}">
+            @csrf
+
+            <div>
+                <x-input-label for="name" :value="__('Name')" />
+                <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', '')" required autofocus autocomplete="name" />
+                <x-input-error class="mt-2" :messages="$errors->get('name')" />
+            </div>
+
+            <div>
+                <x-input-label for="description" :value="__('Description')" />
+                <textarea
+                    name="description"
+                    placeholder="{{ __('Enter your Event description here ...') }}"
+                    class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                >{{ old('message') }}</textarea>
+                <x-input-error :messages="$errors->get('message')" class="mt-2" />
+            </div>
+            <x-primary-button class="mt-4">{{ __('Save') }}</x-primary-button>
+        </form>
+    </div>
+</x-app-layout>
---
+++ resources/views/events/index.blade.php
@@ -0,0 +1,50 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Events') }}
+            <a href="{{ route('events.create') }}">
+                <x-primary-button class="ml-5 mt-4">{{ __('Create Event') }}</x-primary-button>
+            </a>
+        </h2>
+    </x-slot>
+
+    <div class="max-w-1xl mx-auto p-4 sm:p-6 lg:p-8">
+        <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+            <table class="table-auto w-full">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Date</th>
+                        <th>#</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($events as $event)
+                        <tr>
+                            <td>{{ $event->id }}</td>
+                            <td>{{ $event->name }}</td>
+                            <td>{{ $event->description }}</td>
+                            <td>{{ $event->created_at->format('d.m.Y H:i') }}</td>
+                            <td>
+                                <a href="{{ route('events.edit', $event) }}">
+                                    <x-primary-button class="ml-5 mt-4">{{ __('Edit') }}</x-primary-button>
+                                </a>
+
+                                <form method="POST" action="{{ route('events.destroy', $event) }}">
+                                    @csrf
+                                    @method('delete')
+                                    <x-dropdown-link :href="route('events.destroy', $event)"
+                                        onclick="event.preventDefault(); this.closest('form').submit();">
+                                        {{ __('Delete') }}
+                                    </x-dropdown-link>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</x-app-layout>
